024d29bb8a488d23ee4819616fbe4a9e653758e4
Wellington Neto <wellingtonmoreiraneto@hotmail.com>
2025-04-09 14:42:34 -0300

    Adicionando templates HTML e configurando a view home para renderizar o template de recipes

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'recipes/home.html')      
 
 def contato(request):       
     return HttpResponse('contato')
